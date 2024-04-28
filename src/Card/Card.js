68c5736c412bb7d944fa1ade557cf0bb32ec3679
GreyHope <fab.mckay@gmail.com>
2024-04-28 19:46:01 +0200

    ✨💄 Added scryfall button and tweaked style

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -2,6 +2,7 @@ import DefaultLayout from "./CardLayouts/DefaultLayout";
 import FlipLayout from "./CardLayouts/FlipLayout";
 import DoubleFacedLayout from "./CardLayouts/DoubleFacedLayout";
 import CardOracleElement from "./CardOracleElement";
+import { Button } from "@mui/material";
 
 
 function getCardLayout(card, width, height) {
@@ -42,6 +43,15 @@ export default function Card({ card, width, height }) {
     return (
         <div className='card-container'>
             {getCardLayout(card, width, height)}
+            {card !== null ? (<Button
+                variant="contained"
+                size='large'
+                color="secondary"
+                href={card.scryfall_uri}
+                target="_blank"
+                rel="noreferrer noopener">
+                Open on Scryfall
+            </Button>) : ""}
             <CardOracleElement card={card}></CardOracleElement>
         </div>
 
